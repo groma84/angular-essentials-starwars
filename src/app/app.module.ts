ed159f7d99f8456693bec649801ac9aa91839023
Martin Grotz <martin.grotz@redheads.de>
2017-11-20 15:07:49 +0100

    Komponenten angelegt und Bootstrap eingebunden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ListComponent } from './list/list.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListComponent, TabsComponent, ItemComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
