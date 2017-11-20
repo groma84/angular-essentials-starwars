41e1ed2a8d1b952f8098ce78cf81f891a4f3744d
Martin Grotz <martin.grotz@redheads.de>
2017-11-20 15:41:54 +0100

    Erste Komponenten implementiert

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @Input() character;
+  @Output() sideAssigned = new EventEmitter<{ name: string; side: string }>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onAssign(side) {
+    this.sideAssigned.emit({ name: this.character.name, side: side });
   }
-
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: 'light' },
+    { name: 'Darth Vader', side: 'dark' },
+    { name: 'Yoda', side: 'light' }
+  ];
+  chosenList = 'all';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+
+    return this.characters.filter(char => {
+      return char.side === this.chosenList;
+    });
   }
 
+  onChoose(newList) {
+    this.chosenList = newList;
+  }
+
+  onSideAssigned(charInfo) {
+    const pos = this.characters.findIndex(char => {
+      return char.name === charInfo.name;
+    });
+
+    this.characters[pos].side = charInfo.side;
+  }
 }
